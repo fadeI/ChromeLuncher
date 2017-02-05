39f5d3e34925409c30e47359e9abd21fa228183a
fadeI <fadeisma@outlook.com>
2017-02-05 07:51:39 +0200

    add improvement of application

--- ChromeLuncher/ChromeLuncher/Form1.cs
+++ ChromeLuncher/ChromeLuncher/Form1.cs
@@ -5,16 +5,17 @@ using System.IO;
 using System.Net;
 using System.Windows.Forms;
 using System.Reflection;
+using System.Net.NetworkInformation;
 
 namespace ChromeLuncher
 {
     public partial class Form1 : Form
     {
-        Assembly assembly = Assembly.GetExecutingAssembly();
-        String resourceName = "ChromeLuncher.websites.txt";
+
+        const string FileNameChromeLuncher = "ChromeLuncher.txt";
         List<string> websites = new List<string>();
         List<string> addedWebsites = new List<string>();
-        
+
 
         public Form1()
         {
@@ -24,8 +25,11 @@ namespace ChromeLuncher
 
         private void initList()
         {
+            
+            listBox1.DataSource = null;
             listBox1.DataSource = websites;
             listBox1.SelectedIndex = -1;
+            listBox1.Refresh();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -45,49 +49,45 @@ namespace ChromeLuncher
 
         private void button2_Click(object sender, EventArgs e)
         {
-            if (String.IsNullOrEmpty(txt_newWebsite.Text.ToString()) || !isReachable(txt_newWebsite.Text.ToString()))
+            if (string.IsNullOrEmpty(txt_newWebsite.Text.ToString()) || 
+                !isReachable(txt_newWebsite.Text.ToString().Trim()))
             {
                 MessageBox.Show("please valid website");
             }
             else
             {
                 string newWebsite = txt_newWebsite.Text;
-                addedWebsites.Add(newWebsite);
+                websites.Add(newWebsite);
+                initList();
             }
         }
 
         private bool isReachable(string url)
         {
-            try
-            {
-                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
-                if (response == null || response.StatusCode != HttpStatusCode.OK)
-                {
-                    response.Close();
-                    return true;
-                }
-                else
-                {
-                    response.Close();
-                    return false;
-                }
-            }
-            catch (Exception e)
-            {
-                return false;
-            }
-
-
+            var ping = new Ping();
+            var result = ping.Send(url);
+            if (result.Status == IPStatus.Success)
+                return true;
+            return false;
         }
 
 
+        private string getFile()
+        {
+            string driveName = DriveInfo.GetDrives()[1].Name;
+            if (!File.Exists(Path.Combine(driveName, FileNameChromeLuncher)))
+            {
+                File.Create(Path.Combine(driveName, FileNameChromeLuncher));
+            }
+            return Path.Combine(driveName, FileNameChromeLuncher);
+        }
         private void readFromFile()
         {
             try
-            {   // Open the text file using a stream reader.
-                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-                using (StreamReader reader = new StreamReader(stream))
+            {
+                string fileName = getFile();
+                MessageBox.Show(fileName);
+                using (StreamReader reader = new StreamReader(fileName))
                 {
                     string[] result = reader.ReadToEnd().Split('\n');
                     foreach (var s in result)
@@ -110,17 +110,35 @@ namespace ChromeLuncher
             initList();
 
         }
-        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (!e.Cancel)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UpdateFile();
+            e.Cancel = true;
+            this.Dispose();
+        }
+
+        private void UpdateFile()
         {
             try
-            {   // Open the text file using a stream reader.
-                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
-                using (StreamWriter writer = new StreamWriter(stream))
+            {
+                string fileName = getFile();
+                System.IO.File.WriteAllText(fileName, string.Empty);
+                using (StreamWriter writer = new StreamWriter(fileName))
                 {
 
-                    foreach (var s in addedWebsites)
+                    foreach (var s in websites)
                     {
                         writer.Write(s);
+                        writer.Write("\n");
                     }
                 }
             }
@@ -131,4 +149,6 @@ namespace ChromeLuncher
             }
         }
     }
+
+
 }
